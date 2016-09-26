64acab2119d9224932ded87194af3683b791bad3
Alexandra Poltorina <poltorina96@gmail.com>
2016-09-27 01:01:25 +0300

    600

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
-
+	var imgHeight = $('.clients .carousel .slider-nav img').height();
+	imgHeight = imgHeight / 4.57;
+	$('.clients .carousel .slider-nav').css('top', imgHeight + 'px');
 
 });	
